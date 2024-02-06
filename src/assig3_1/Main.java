b6e700413d4e9842d243e913a38a9d4923e9ef30
Yagel Atias <yagelatias@outlook.com>
2024-02-06 15:48:30 +0200

    turn variable, each thread runs 3 times

--- src/assig3_1/Main.java
+++ src/assig3_1/Main.java
@@ -1,12 +1,63 @@
 package assig3_1;
 
 public class Main {
-    public static void main(String[] args) {
-        Thread t1 = new Thread(new MyThread("t1"));
-        Thread t2 = new Thread(new MyThread("t2"));
-        Thread t3 = new Thread(new MyThread("t3"));
+    static volatile int turn = 1;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread() {
+            public void run() {
+                while (true) {
+                    while(turn != 1);
+                    for(int i = 0; i < 3; i++){
+                        System.out.println("A is running");
+                        try {
+                            sleep(1000);
+                        } catch (InterruptedException e) {
+                            System.out.println("A was interrupted");
+                        }
+                    }
+                    turn = 2;
+                }
+            }
+        };
+
+        Thread t2 = new Thread(){
+            public void run(){
+                while(true){
+                    while(turn != 2);
+                    for(int i = 0; i < 3; i++){
+                        System.out.println("B is running");
+                        try {
+                            sleep(1000);
+                        } catch (InterruptedException e) {
+                            System.out.println("B was interrupted");
+                        }
+                    }
+                    turn = 3;
+                }
+            }
+        };
+
+        Thread t3 = new Thread(){
+            public void run(){
+                while (true) {
+                    while(turn != 3);
+                    for(int i = 0; i < 3; i++){
+                        System.out.println("C is running");
+                        try {
+                            sleep(1000);
+                        } catch (InterruptedException e) {
+                            System.out.println("C was interrupted");
+                        }
+                    }
+                    turn = 1;
+                }
+            }
+        };
+
         t1.start();
         t2.start();
         t3.start();
+        t1.join();
     }
 }
