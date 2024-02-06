6f1dc6546d7803fe0fd6aaa0baa4c505af83412b
Yagel Atias <yagelatias@outlook.com>
2024-02-06 16:01:54 +0200

    Improved readabillity, added methods

--- src/assig3_1/Main.java
+++ src/assig3_1/Main.java
@@ -1,63 +1,37 @@
 package assig3_1;
 
 public class Main {
-    static volatile int turn = 1;
+    static volatile int currentTurn = 1;
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread() {
-            public void run() {
-                while (true) {
-                    while(turn != 1);
-                    for(int i = 0; i < 3; i++){
-                        System.out.println("A is running");
-                        try {
-                            sleep(1000);
-                        } catch (InterruptedException e) {
-                            System.out.println("A was interrupted");
-                        }
-                    }
-                    turn = 2;
-                }
-            }
-        };
-
-        Thread t2 = new Thread(){
-            public void run(){
-                while(true){
-                    while(turn != 2);
-                    for(int i = 0; i < 3; i++){
-                        System.out.println("B is running");
-                        try {
-                            sleep(1000);
-                        } catch (InterruptedException e) {
-                            System.out.println("B was interrupted");
-                        }
-                    }
-                    turn = 3;
-                }
-            }
-        };
-
-        Thread t3 = new Thread(){
-            public void run(){
-                while (true) {
-                    while(turn != 3);
-                    for(int i = 0; i < 3; i++){
-                        System.out.println("C is running");
-                        try {
-                            sleep(1000);
-                        } catch (InterruptedException e) {
-                            System.out.println("C was interrupted");
-                        }
-                    }
-                    turn = 1;
-                }
-            }
-        };
+        Thread t1 = createThread('A', 1, 2);
+        Thread t2 = createThread('B', 2, 3);
+        Thread t3 = createThread('C', 3, 1);
 
         t1.start();
         t2.start();
         t3.start();
         t1.join();
+    }
+
+    private static Thread createThread(char name, int waitTurn, int nextTurn) {
+        return new Thread(() -> {
+            while (true) {
+                while (currentTurn != waitTurn) ;
+                printStatusAndSleep(name);
+                currentTurn = nextTurn;
+            }
+        });
+    }
+
+    private static void printStatusAndSleep(char name) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(name + " is running");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(name + " was interrupted");
+            }
+        }
     }
 }
