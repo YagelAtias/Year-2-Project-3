1dbfa44e8fb6519ce6ab1de175e16d84931d7dcd
Yagel Atias <yagelatias@outlook.com>
2024-02-09 15:37:00 +0200

    Added main method for Q2 with Gamer names

--- src/assig3_2/Main.java
+++ src/assig3_2/Main.java
@@ -1,10 +1,10 @@
 package assig3_2;
 
-public class Main {
+public class    Main {
     public static void main(String[] args) {
         GamePlay game = new GamePlay();
-        Gamer gamer = new Gamer(game, "Ron");
-        Gamer gamer2 = new Gamer(game, "Noe");
+        Gamer gamer = new Gamer(game, "Slava");
+        Gamer gamer2 = new Gamer(game, "Yagel");
         Judge judge = new Judge(game);
         gamer.start();
         gamer2.start();
